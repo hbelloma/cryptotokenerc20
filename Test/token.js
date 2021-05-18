47a248924f6bb934148e45358d79230b6cc51905
Hector Bello Martinez <62455718+hbelloma@users.noreply.github.com>
2021-05-18 02:41:33 -0500

    Update token.js

--- Test/token.js
+++ Test/token.js
@@ -1,12 +1,13 @@
 
 
 //const Token = artifacts.require('ETBToken.sol');
-const Token = artifacts.require('TokenERC20.sol');
+//const Token = artifacts.require('TokenERC20.sol');
+const Token = artifacts.require('DFeynmanCoin.sol');
 
 contract('Token', accounts=> {
   let token;
   const [admin, _] = accounts;
-  const TOTAL_SUPPLY = web3.utils.toWei('1000000');
+  const TOTAL_SUPPLY = web3.utils.toWei('207542'); //ammount for airdrop
 
   before(async () => {
     token = await Token.new();
