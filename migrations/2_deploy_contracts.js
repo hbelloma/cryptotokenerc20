7fe3e74cfb528b7d151e3784cd9f6e5ba017eb05
Hector Bello Martinez <62455718+hbelloma@users.noreply.github.com>
2021-05-18 03:03:33 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
-const ETBToken = artifacts.require('ETBToken.sol');
+//const ETBToken = artifacts.require('ETBToken.sol');
+const DFeynmanCoin  = artifacts.require('DFeynmanCoin.sol');
 
 module.exports = function (deployer) {
   deployer.deploy(ETBToken);
